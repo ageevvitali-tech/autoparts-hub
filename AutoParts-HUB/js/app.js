71f6e429116c60cfc3a1a080918abee4c90f96d0
ageevvitali-tech <ageevvitali@gmail.com>
2026-09-07 20:15:59 +0300

    Delete app.js

--- AutoParts-HUB/js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("AutoParts-HUB запущен");
-
-    const yearElement = document.querySelector("[data-year]");
-
-    if (yearElement) {
-        yearElement.textContent = new Date().getFullYear();
-    }
-});
